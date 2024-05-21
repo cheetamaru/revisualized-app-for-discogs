5e6a0f0d02ccc7a8d0e2ddde5fad46c0848d7587
cheetamaru <vitalvat@gmail.com>
2024-05-21 21:33:14 +0300

    feat: add workaround for image retrieveing

--- src/service/api/ApiClient.ts
+++ src/service/api/ApiClient.ts
@@ -3,10 +3,12 @@ import { DiscogsClient } from '@lionralfs/discogs-client';
 
 const createClient = () => {
     const client = new DiscogsClient({
+        userAgent: 'DiscogsRevisualized/1.0',
         auth: {
             method: 'discogs',
             consumerKey: getConsumerKey(),
             consumerSecret: getConsumerSecret(),
+            userToken: process.env.DISCOGS_PERSONAL_TOKEN,
         },
     });
     

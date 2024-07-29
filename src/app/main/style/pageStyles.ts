0c41e947fb1a5b4e1f3d2b5e41b441c7a4314c30
cheetamaru <vitalvat@gmail.com>
2024-07-29 22:54:59 +0300

    feat: move page style to a file

---
+++ src/app/main/style/pageStyles.ts
@@ -0,0 +1,21 @@
+const boxStyle: React.CSSProperties = {
+    width: '100%',
+    height: '100vh',
+  };
+  
+  const titleContainerStyle: React.CSSProperties = {
+    display: 'flex',
+    justifyContent: "center"
+  }
+  
+  const titleStyle: React.CSSProperties = {
+    color: "white",
+    textAlign: "center",
+    fontSize: 44
+  }
+
+export const pageStyles = {
+    boxStyle,
+    titleContainerStyle,
+    titleStyle
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,13 @@ import { Flex } from "antd";
 import style from "./page.module.css"
 import Title from "antd/es/typography/Title";
 import RoutedUserSearcher from "@/app/user/ui/components/UserSearcher/RoutedUserSearcher";
+import { pageStyles } from "./main/style/pageStyles";
 
-const boxStyle: React.CSSProperties = {
-  width: '100%',
-  height: '100vh',
-};
-
-const titleContainerStyle: React.CSSProperties = {
-  display: 'flex',
-  justifyContent: "center"
-}
-
-const titleStyle: React.CSSProperties = {
-  color: "white",
-  textAlign: "center",
-  fontSize: 44
-}
+const {
+  boxStyle,
+  titleContainerStyle,
+  titleStyle
+} = pageStyles;
 
 export default function Home() {
   return (

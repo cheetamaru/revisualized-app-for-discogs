1b6801a017f638e9bc26f0b6d05e903199da31b8
cheetamaru <vitalvat@gmail.com>
2024-08-20 20:35:14 +0300

    feat: add dockerfile and dockerignore [wp]

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: process.env.DISCOGS_IMAGE_HOSTNAME,
-          },
-          {
-            protocol: "https",
-            hostname: process.env.DISCOGS_GRAVATAR_HOSTNAME,
-          },
-          {
-            protocol: "https",
-            hostname: process.env.DISCOGS_GRAVATAR_DEFAULT_HOSTNAME,
-          },
-          {
-            protocol: "https",
-            hostname: process.env.NEXT_PUBLIC_DISCOGS_HOSTNAME
-          }
-        ]
-    }
+  output: "standalone",
+  images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: process.env.DISCOGS_IMAGE_HOSTNAME,
+        },
+        {
+          protocol: "https",
+          hostname: process.env.DISCOGS_GRAVATAR_HOSTNAME,
+        },
+        {
+          protocol: "https",
+          hostname: process.env.DISCOGS_GRAVATAR_DEFAULT_HOSTNAME,
+        },
+        {
+          protocol: "https",
+          hostname: process.env.NEXT_PUBLIC_DISCOGS_HOSTNAME
+        }
+      ]
+  }
 };
 
 export default nextConfig;

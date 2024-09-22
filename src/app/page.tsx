8120848c7b7d0577afaceeee53219e8728d83fa1
cheetamaru <vitalvat@gmail.com>
2024-09-22 21:51:14 +0300

    feat: add a collapse panel to the mai page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Flex } from "antd";
+import { Collapse, CollapseProps, Flex } from "antd";
 import style from "./main/style/page.module.css"
 import Title from "antd/es/typography/Title";
 import RoutedUserSearcher from "@/app/user/ui/components/UserSearcher/RoutedUserSearcher";
@@ -11,6 +11,17 @@ const {
 } = pageStyles;
 
 export default function Home() {
+  const text = `The purpose of the site is to show other people you wantlist of music mediums.
+  To use this site you should have the username of a Discogs.com user with an open account`
+
+  const items: CollapseProps['items'] = [
+    {
+      key: '1',
+      label: 'How to use this site?',
+      children: <p>{text}</p>,
+    },
+  ];
+
   return (
     <main>
       <Flex style={boxStyle} justify="center" align="center">
@@ -25,6 +36,9 @@ export default function Home() {
           
           <div className={style.username_input}>
             <RoutedUserSearcher routerPathBeforeUsername="/wantlist/" />
+          </div>
+          <div>
+            <Collapse items={items} ghost />
           </div>
         </div>
       </Flex>

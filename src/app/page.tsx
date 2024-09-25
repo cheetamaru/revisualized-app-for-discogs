4a8918a7278ccd6166c168593ac5968b4412dbfb
cheetamaru <vitalvat@gmail.com>
2024-09-25 20:57:49 +0300

    feat: move page content to a separate component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,9 @@
-import { Flex } from "antd";
-import style from "./main/style/page.module.css"
-import RoutedUserSearcher from "@/app/user/ui/components/UserSearcher/RoutedUserSearcher";
-import { pageStyles } from "./main/style/pageStyles";
-import MainPageTitle from "./main/ui/MainPageTitle";
-import MainPageHelp from "./main/ui/MainPageHelp";
-
-const {
-  boxStyle,
-} = pageStyles;
+import MainPage from "./main/ui/MainPage";
 
 export default function Home() {
   return (
     <main>
-      <Flex style={boxStyle} justify="center" align="center">
-        <div>
-          <MainPageTitle />
-
-          <div className={style.username_input}>
-            <RoutedUserSearcher routerPathBeforeUsername="/wantlist/" />
-          </div>
-
-          <div className={style.help}>
-            <MainPageHelp />
-          </div>
-        </div>
-      </Flex>
+      <MainPage />
     </main>
   );
 }

6d6f7a1a433a73ad3cf317346c7389a2676c4fa9
cheetamaru <vitalvat@gmail.com>
2024-10-03 20:28:17 +0300

    feat: add loading state to the card

--- src/app/resource/ui/components/ResourceEntryCardCover/ResourceEntryCardCover.tsx
+++ src/app/resource/ui/components/ResourceEntryCardCover/ResourceEntryCardCover.tsx
@@ -5,12 +5,14 @@ import { ResourceFormatImageDomain } from "../../../domain/ResourceFormatImageDo
 import ResourceEntryCardCoverImage from "./ResourceEntryCardCoverImage";
 import ResourceEntryCardFormatImage from "./ResourceEntryCardFormatImage";
 import { ResourceEntryCardCoverDomain } from "@/app/resource/domain/ResourceEntryCardCoverDomain";
+import SkeletonImage from "antd/es/skeleton/Image";
 
 type Props = {
     src: string;
     coverImageHeight: number;
     format: MusicEntryFormat;
     title: string;
+    loading?: boolean;
 }
 
 const { getFormatImageSrc, isFileFormat } = ResourceFormatImageDomain;
@@ -50,7 +52,7 @@ const getContainerStyle = ({
     }
 }
 
-const ResourceEntryCardCover = ({src, coverImageHeight, title, format}: Props) => {
+const ResourceEntryCardCover = ({src, coverImageHeight, title, format, loading}: Props) => {
     const { name: formatName, quantity, descriptions } = format;
 
     const { onePart, coverSize } = getCoverGridOptions(coverImageHeight)
@@ -59,6 +61,15 @@ const ResourceEntryCardCover = ({src, coverImageHeight, title, format}: Props) =
 
     const isDescriptionShown = Boolean(formatImg) || isFileFormat(formatName)
     const isQuantityShown = Boolean(formatImg && quantity > 1)
+
+    if (loading) {
+        return <div 
+            style={getContainerStyle({ onePart, coverSize })} 
+            className={style.cover_container}
+        >
+            <SkeletonImage active />
+        </div>
+    }
 
     return (
     <>
